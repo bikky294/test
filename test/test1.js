be8b7e88792067bc74349d39f36054029c55a4d2
bikky294 <bishalharizan9@gmail.com>
2024-10-07 10:59:22 +0000

    done

--- test/test1.js
+++ test/test1.js
@@ -4,24 +4,7 @@ const box = document.querySelectorAll(".box");
 box.forEach(function(box){
     
    box.addEventListener("click",  (event)=>{
-  
-    if(box.id==="red"){
-       body.style.backgroundColor=box.id;
-       
-      
-    }
-    else if(box.id==="green")
-    {
-        body.style.backgroundColor=box.id;
-    }
-   else if(box.id==="blue")
-    {
-        body.style.backgroundColor=box.id;
-    }
-   else if(box.id==="pink")
-    {
-        body.style.backgroundColor=box.id;
-    }
-   
+   body.style.backgroundColor=event.target.id;
+    
    })
 })
