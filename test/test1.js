7e23538b638cb22f70e2cdf974827fa676d2eae6
bikky294 <bishalharizan9@gmail.com>
2024-10-07 10:43:46 +0000

    done

--- test/test1.js
+++ test/test1.js
@@ -1,1 +1,27 @@
-console.log("hello  world")
+const body = document.querySelector("body");
+const box = document.querySelectorAll(".box");
+
+box.forEach(function(box){
+    
+   box.addEventListener("click",  (event)=>{
+  
+    if(box.id==="red"){
+       body.style.backgroundColor=box.id;
+       
+      
+    }
+    else if(box.id==="green")
+    {
+        body.style.backgroundColor=box.id;
+    }
+   else if(box.id==="blue")
+    {
+        body.style.backgroundColor=box.id;
+    }
+   else if(box.id==="pink")
+    {
+        body.style.backgroundColor=box.id;
+    }
+   
+   })
+})
